3289ccb65a8f8c572e7e20006fff6565896d8716
BuffetAgrario <prediojudicialtsas@gmail.com>
2026-02-14 14:04:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'buffet-cache-v1.0.2';
+const CACHE_NAME = 'buffetCache-v2';
 // Rutas a cachear al instalar el SW. Ajusta si cambias nombres.
 const URLS_TO_CACHE = [
   './',          // carpeta actual
